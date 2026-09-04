edc85bd38033ecaafeebef601553ef65c1447ad4
ummeaimen <hafiza.aimen786@gmail.com>
2026-09-04 14:35:30 +0500

    once login redirect to home

--- turbine_app.py
+++ turbine_app.py
@@ -1,35 +1,60 @@
 from flask import Flask, render_template
-from flask import request,redirect,url_for
+
+from flask import request,redirect,url_for,session
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.secret_key = os.getenv("secret_key")
+
 user = {"password": "admin"}
 
-# @app.route('/')
-# def index():
-#     return render_template('login.html')
+
 
 @app.route('/login', methods=['get','POST'])
 
 def login():
 
+    if session.get('logged in'):
+                
+        return redirect(url_for('home'))
+
+    
     if request.method == 'POST':
 
         password = request.form['password']
 
         if password == user['password']:
 
+            session['logged in'] = True
+
             return redirect(url_for('home'))
         
         else:
 
             return render_template('login.html',message="Invalid password")
+        
+    
+        
     return render_template('login.html')
+
+    
+
+        
 
 @app.route('/home')
 
 def home():
 
+    if not session.get('logged in'):
+
+        return redirect(url_for('login'))
+    
     return render_template('home.html')
 
 
